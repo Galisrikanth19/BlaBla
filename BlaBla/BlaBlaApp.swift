5ce8a7cbf6e5c41ff86addc7e60a97df262ae17c
GaliSrikanth <GaliSrikanth19@gmail.com>
2025-05-06 12:08:20 +0530

    Initial Commit

---
+++ BlaBla/BlaBlaApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlaBlaApp.swift
+//  BlaBla
+//
+//  Created by GaliSrikanth on 06/05/25.
+//
+
+import SwiftUI
+
+@main
+struct BlaBlaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
